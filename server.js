60b71f7058596ffc548649cb6c97aad7bb4b5bc1
WellingtonSilva448 <wellingtonsilvadeoliveira448@gmail.com>
2026-03-23 19:28:54 -0300

    feat: adiciona busca por ID (GET) e cadastro de incidentes (POST)

--- server.js
+++ server.js
@@ -27,3 +27,20 @@ app.get('/incidentes', async (req, res) => {
     const listaIncidentes = await db.all(`SELECT * FROM incidentes`)
     res.json(listaIncidentes)
 })
+
+app.get('/incidentes/:id', async (req, res) => {
+    const { id } = req.params
+    const db = await criarBanco()
+    const incidentesEspecifico = await db.all(`SELECT * FROM incidentes WHERE id = ?`, [id])
+    res.json(incidentesEspecifico)
+
+})
+
+app.post('/incidentes', async (req, res) => {
+    const {tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro} = req.body
+    const db = await criarBanco()
+    await db.run(`INSERT INTO incidentes(tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro) VALUES (?, ?, ?, ?, ?, ?, ?)`, [tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro])
+    //resposta de confirmação para o cliente que fez a requisição
+    res.send(`Incidente novo registrado ${tipo_problema} registrado na data ${data_registro} por ${nome_solicitante}`)
+
+})
